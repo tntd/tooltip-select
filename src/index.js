645d033b6a6f54d817106d66acb182e8aa4f269d
huatao.zhang <337198954@qq.com>
2023-09-05 16:59:25 +0800

    修复搜索和多dom场景

--- src/index.js
+++ src/index.js
@@ -1,31 +1,81 @@
 import { Select, Tooltip, TntdSelect } from 'tntd';
-import { memo, useCallback, useMemo, useState } from 'react';
-import { isEqual } from 'lodash';
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
+import { isArray, isEqual } from 'lodash';
 
 const TooltipSelect = memo(
     (props) => {
-        const { children, value, isVirtual = false, placement = 'top', setTitle } = props;
+        const { children, value, isVirtual = false, placement = 'top', setTitle, optionFilterProp, filterOption } = props;
 
         let Option = isVirtual ? TntdSelect.Option : Select.Option
-        // //获取添加Tooltip的option子项
-        let tooltipChildren = children?.map(item => {
-            return (
-                <Option {...item?.props}>
-                    <Tooltip title={setTitle ? setTitle(item?.props?.children) : item?.props?.children}>
-                        <span style={{ marginRight: "5px" }}>
-                            {item?.props?.children}
-                        </span>
-                    </Tooltip>
-                </Option>
-            );
-        });
+        let originalFilterOption = filterOption;  // 将原始的filterOption函数引用保存到一个新的变量
 
+
+        const filterOptionFunction = (input, option) => {
+            // 创建一个新的option对象，其中props.children是由props.originChildren替换的
+            const newOption = {
+                ...option,
+                props: {
+                    ...option.props,
+                    children: option.props.originChildren
+                }
+            };
+
+            // 调用原始的filterOption函数
+            return filterOption(input, newOption);
+        };
+        // //获取添加Tooltip的option子项
+
+        let tooltipChildren = []
+
+
+        tooltipChildren = children?.map(item => {
+            if (isArray(item)) {
+                return item?.map(item1 => {
+                    if (item1?.props?.value !== undefined && item1?.props?.value !== null) {
+                        return (
+                            <Option {...item1?.props} originChildren={item1?.props.children}>
+                                <Tooltip title={setTitle ? setTitle(item1?.props?.children) : item1?.props?.children}>
+                                    <span style={{ marginRight: "5px" }}>
+                                        {item1?.props?.children}
+                                    </span>
+                                </Tooltip>
+                            </Option>
+                        );
+                    } else {
+                        return false
+                    }
+                })
+            } else {
+                if (item?.props?.value !== undefined && item?.props?.value !== null) {
+                    return (
+                        <Option {...item?.props} originChildren={item?.props.children}>
+                            <Tooltip title={setTitle ? setTitle(item?.props?.children) : item?.props?.children}>
+                                <span style={{ marginRight: "5px" }}>
+                                    {item?.props?.children}
+                                </span>
+                            </Tooltip>
+                        </Option>
+                    );
+                } else {
+                    return false
+                }
+
+            }
+        });
 
         return (
             <>
                 {isVirtual ?
-                    <TntdSelect {...props} >{tooltipChildren}</TntdSelect>
-                    : <Select {...props} >{tooltipChildren}</Select>}
+                    <TntdSelect
+                        {...props}
+                        optionFilterProp={optionFilterProp === "children" ? "originChildren" : optionFilterProp}
+                        filterOption={filterOptionFunction}>{tooltipChildren}
+                    </TntdSelect>
+                    : <Select
+                        {...props}
+                        optionFilterProp={optionFilterProp === "children" ? "originChildren" : optionFilterProp}
+                        filterOption={filterOptionFunction}>{children}
+                    </Select>}
             </>
         );
     },
--- test/index.js
+++ test/index.js
@@ -10,14 +10,20 @@ const Demo = (props) => {
   const aTitle = (title) => {
     return <a>{title}</a>;
   };
+  let words = ["22", "苹果", "橙子", "香蕉", "柠檬", "蓝莓", "草莓", "樱桃", "葡萄", "番石榴", "西瓜", "杏子", "李子", "桃子", "梨", "芒果", "木瓜", "红枣", "桑葚", "猕猴桃", "柿子"]
+
   return (
-    <TooltipSelect
+    <Select
       dropdownMatchSelectWidth={false}
       isVirtual
       value={title}
+      optionFilterProp="children"
       setTitle={(value) => {
         return <a><div style={{ width: "5px", height: "5px", background: "red", display: "inline-block" }}></div>{value}</a>
       }}
+      // filterOption={(input, option) =>
+      //   (Array.isArray(option.props.children) ? option.props.children.join("") : option.props.children).toLowerCase().indexOf(input.toLowerCase()) >= 0}
+
       onChange={(value) => {
         setTitle(value);
       }}
@@ -25,13 +31,29 @@ const Demo = (props) => {
       allowClear
       showSearch
     >
-      {new Array(10000).fill(0)?.map((item, index) => (
+      {/* {true && new Array(1000).fill(0)?.map((item, index) => (
         <Option key={index} value={index}>
+          {false && 1}
+          {true && <div>{index}</div>}
+          {true && <div>{Math.random(10) + Math.random(1) * 0.00001}</div>}
 
-          <div>{Math.random(10) + Math.random(1) * 0.00001}</div>
         </Option>
       ))}
-    </TooltipSelect>
+      {false && new Array(1000).fill(0)?.map((item, index) => (
+        <Option key={index} value={index}>
+          {false && 1}
+          {true && <div>{index}</div>}
+          {true && <div>{Math.random(10) + Math.random(1) * 0.00001}</div>}
+
+        </Option>
+      ))} */}
+      {words?.map((item, index) => (
+        <Option key={index} value={index}>
+          <sup >111</sup>
+          {item}
+        </Option>
+      ))}
+    </Select>
   );
 };
 
